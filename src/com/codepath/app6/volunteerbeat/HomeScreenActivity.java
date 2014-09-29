a70c49f1e2d6fc6af4a9772ee734116a7d156760
ppatibandla <patibandla@gmail.com>
2014-09-28 23:37:43 -0700

    First Commit

---
+++ src/com/codepath/app6/volunteerbeat/HomeScreenActivity.java
@@ -0,0 +1,15 @@
+package com.codepath.app6.volunteerbeat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HomeScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
