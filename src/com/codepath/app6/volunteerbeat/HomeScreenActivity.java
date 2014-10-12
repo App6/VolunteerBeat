dcdb7e9e4d163f100e9565a82e1dfb3ca48e5403
vvayv <liuqingdi@gmail.com>
2014-10-11 17:01:56 -0700

    init. check in for user profile page

--- src/com/codepath/app6/volunteerbeat/HomeScreenActivity.java
+++ src/com/codepath/app6/volunteerbeat/HomeScreenActivity.java
@@ -1,9 +1,13 @@
 package com.codepath.app6.volunteerbeat;
 
+import com.codepath.app6.volunteerbeat.activities.ProfileActivity;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class HomeScreenActivity extends Activity {
 
@@ -11,5 +15,10 @@ public class HomeScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+    
+    public void viewProfile(View v) {
+    	Intent i = new Intent(this, ProfileActivity.class);
+    	startActivity(i);
     }
 }
