8f8d693159f69bb3612bacb506ecd20f9b45fea0
ppatibandla <patibandla@gmail.com>
2014-10-11 21:00:18 -0700

    * Actionbar theme. * Profile image in Action bar. * Circular image in Action bar.

--- src/com/codepath/app6/volunteerbeat/HomeScreenActivity.java
+++ src/com/codepath/app6/volunteerbeat/HomeScreenActivity.java
@@ -1,15 +1,46 @@
 package com.codepath.app6.volunteerbeat;
 
-import android.app.Activity;
+import com.mikhaellopez.circularimageview.CircularImageView;
+import com.squareup.picasso.Picasso;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 
-public class HomeScreenActivity extends Activity {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
-    }
+public class HomeScreenActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home_screen);
+
+		final ActionBar actionBar = getSupportActionBar();
+		actionBar.setCustomView(R.layout.action_profile_view);
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setDisplayShowCustomEnabled(true);
+		/*Replace this with the custom Image location on disk*/
+//		String url = "http://2.gravatar.com/avatar/858dfac47ab8176458c005414d3f0c36?s=256&d=&r=G";
+		String url = "http://a5.files.biography.com/image/upload/c_fill,dpr_1.0,g_face,h_300,q_80,w_300/MTE4MDAzNDEwNzg5ODI4MTEw.jpg";
+		CircularImageView ivactionbarLogo = (CircularImageView) actionBar.getCustomView().findViewById(R.id.actionBarLogo);
+/*		ivactionbarLogo.setBorderColor(getResources().getColor(R.color.vb_red));
+		ivactionbarLogo.setBorderWidth(10);
+		ivactionbarLogo.addShadow();
+		*/
+		Picasso.with(this)
+				.load(url)
+//				.resize(30, 30)
+				.into(ivactionbarLogo);
+	
+		ivactionbarLogo.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(HomeScreenActivity.this, "Profile Click", Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
 }
